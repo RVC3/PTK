52a48e27c1dbea830780c77b1e86ad6dfb826528
Cvostr <cvosttv@gmail.com>
2020-07-13 17:12:40 +0300

    AscReader9000S - authentificate func implemented

--- devices/rfidreal/src/main/java/ru/ppr/rfidreal/AscReader_9000S.java
+++ devices/rfidreal/src/main/java/ru/ppr/rfidreal/AscReader_9000S.java
@@ -4,8 +4,14 @@ import android.content.Context;
 import android.device.IccManager;
 import android.support.annotation.NonNull;
 
+import fr.coppernic.cpcframework.cpcask.Commands;
+import fr.coppernic.cpcframework.cpcask.OnGetReaderInstanceListener;
+import fr.coppernic.cpcframework.cpcask.Reader;
 import fr.coppernic.cpcframework.cpcask.Defines;
 import ru.ppr.logger.Logger;
+import ru.ppr.utils.CommonUtils;
+
+import android.os.SystemClock;
 
 /**
  * Реализация обертки над считывателем смарт-карт от Coppernic SDK для i9000S
@@ -34,6 +40,13 @@ public class AscReader_9000S implements IAscReader {
 
     public AscReader_9000S(Context context) {
         Logger.info(TAG, "AscReader_9000s() constructor START");
+        //Создать объект класса Reader
+        Reader.getInstance(context, new OnGetReaderInstanceListener() {
+            @Override
+            public void OnGetReaderInstance(Reader reader) {
+                Logger.info(TAG, "Constructor() Reader getInstance Done");
+            }
+        });
 
         //Создать экземпляр класса IccManager
         mIccManager = new IccManager();
@@ -201,6 +214,37 @@ public class AscReader_9000S implements IAscReader {
 
     @Override
     public boolean mifareSamNxpAuthenticate(byte numKey, byte versionKey, byte keyAorB, byte numBlock) {
+        long timer = System.currentTimeMillis();
+        //номер блока для авторизации - любой блок в секторе, например нулевой
+        byte blockNumber = 0;
+        byte[] statusCard = new byte[1];
+        short[] statusSam = new short[1];
+        byte lgDiversifier = (byte) 0;
+        byte blockDiversifier = (byte) 0;
+
+        Commands.iMIFARE_SAMNXP_Authenticate(numKey, versionKey, keyAorB, numBlock, lgDiversifier, blockDiversifier);
+
+        byte[] rspBuf = new byte[256];
+        byte[] rspStatus = new byte[2];
+        mIccManager.apduTransmit(Commands.giCSCTrame, Commands.giCSCTrame.length, rspBuf, rspStatus);
+        //lastResult = mReader.mifareSamNxpAuthenticate(numKey, versionKey, keyAorB, numBlock, lgDiversifier, blockDiversifier, statusCard, statusSam);
+        //boolean out = lastResult == Defines.RCSC_Ok && statusSam[0] == (short) 0x9000;
+
+
+
+        /*addLog("mifareSamNxpAuthenticate(" +
+                "numKey=" + numKey +
+                ", versionKey=" + versionKey +
+                ", keyAorB=" + getHex(keyAorB) +
+                ", versionKey=" + getHex(versionKey) +
+                ", numBlock=" + numBlock +
+                ", lgDiversifier=" + lgDiversifier +
+                ", blockDiversifier=" + blockDiversifier +
+                ") DONE {" + getResString(lastResult) +
+                ", statusCard=" + getHex(statusCard) +
+                ", statusSam[0]=" + statusSam[0] +
+                "} " + getResString(out) + getTimeString(timer));*/
+        //return out;
         return false;
     }
 
@@ -211,6 +255,49 @@ public class AscReader_9000S implements IAscReader {
 
     @Override
     public boolean mifareAuthenticate(byte numSector, byte keyAorB, byte keyIndex) {
+        long timer = System.currentTimeMillis();
+
+        byte[] mifareType = {0x00};
+        byte[] serialNumber = new byte[4];
+        byte[] status = new byte[1];
+
+        Commands.iMIFARE_Authenticate(numSector, keyAorB, keyIndex);
+
+        byte[] rspBuf = new byte[256];
+        byte[] rspStatus = new byte[2];
+        mIccManager.apduTransmit(Commands.giCSCTrame, Commands.giCSCTrame.length, rspBuf, rspStatus);
+        int vRet = rspStatus[0];
+
+        Logger.info(TAG, "RSP STATUS 0 " + String.valueOf(rspStatus[0]));
+        Logger.info(TAG, "RSP STATUS 1 " + String.valueOf(rspStatus[1]));
+
+        //lastResult = mReader.mifareAuthenticate(numSector, keyAorB, keyIndex, mifareType, serialNumber, status);
+        boolean out = lastResult == Defines.RCSC_Ok && status[0] == 0;
+
         return false;
+    }
+
+    private void addLog(String text) {
+        Logger.trace(TAG, text);
+    }
+
+    private String getResString(int res) {
+        return "res=" + Defines.errorLookUp(res);
+    }
+
+    private String getResString(boolean res) {
+        return (res) ? "OK" : "FAILED";
+    }
+
+    private String getHex(byte[] value) {
+        return CommonUtils.bytesToHexWithoutSpaces(value);
+    }
+
+    private String getHex(byte value) {
+        return CommonUtils.byteToHex(value);
+    }
+
+    private String getTimeString(long startTimeStamp) {
+        return " - " + (System.currentTimeMillis() - startTimeStamp) + "mc";
     }
 }
